079613fe2f470472d3fe4f06bec0e2c1b855e70f
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-06-27 14:53:03 +0530

    initial commit

---
+++ SwiftUILayoutProtocol/SwiftUILayoutProtocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILayoutProtocolApp.swift
+//  SwiftUILayoutProtocol
+//
+//  Created by Karthik K Manoj on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILayoutProtocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
